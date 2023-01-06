775b4a521b1ac2f622630692fc8a44605e214d18
Flügerl <boostedfluegerl@gmail.com>
2023-01-06 17:05:59 +0100

    Finished syncer.js implementation

--- index.js
+++ index.js
@@ -4,18 +4,23 @@ const {syncPlaylist} = require("./src/syncer");
 
 (async () => {
     const {url} = await inquirer.prompt([{
-        type: 'input', name: 'url', message: 'Which YouTube playlist to import?'
+        type: 'input', name: 'url', message: 'Which YouTube playlist to import?', default: 'https://www.youtube.com/watch?v=FsSBXr5sOcc&list=PL11SmZAO8BYZh7QQYncE4zQrTXkt3XAt1&index=93'
     }]);
 
     const startIdx = url.indexOf('list=') + 5;
     const endIdx = url.indexOf('&', startIdx);
     const id = url.substring(startIdx, endIdx < 0 ? url.length : endIdx);
 
+    if(!id){
+        console.log('No playlist ID found');
+        return;
+    }
+
     const pl = await getPlaylistDetails(id);
 
     const {title, description} = await inquirer.prompt([
         {type: 'input', name: 'title', message: 'Spotify playlist title:', default: pl.title + ' by ' + pl.channelTitle},
-        {type: 'input', name: 'description', message: 'Spotify playlist description:', default: 'Created with https://github.com/vinzynth/spotify-syncer'},
+        {type: 'input', name: 'description', message: 'Spotify playlist description:', default: `Created from https://www.youtube.com/playlist?list=${id} with https://github.com/vinzynth/spotify-syncer`},
     ]);
 
     await syncPlaylist(id, title, description);
--- src/syncer.js
+++ src/syncer.js
@@ -1,6 +1,7 @@
 const {getPlaylist, fetchMusicMetadata} = require("./youtubeApi");
 const replaceSpecialCharacters = require('replace-special-characters');
 const {normalizeText, normalizeDiacritics, normalizeName, normalizeWhiteSpaces} = require('normalize-text');
+const spotifyApi = require("./spotifyApi");
 
 async function syncPlaylist(playlistId, title, description) {
     console.log('Fetching playlist from YouTube');
@@ -13,8 +14,104 @@ async function syncPlaylist(playlistId, title, description) {
     for (const v of playlistWithMetadata)
         v.cleanedTitle = normalizeText(normalizeDiacritics(normalizeName(normalizeWhiteSpaces(replaceSpecialCharacters(v.title)))));
 
+    console.log('Finding best candidate for individual video');
+    const matches = {};
+    for (const v of playlistWithMetadata) {
+        const t = v.title.toLowerCase();
+        if (t === 'private video' || t === 'deleted video')
+            continue;
 
-    debugger;
+        const s = t + ' ' + v.musicMetadata.reduce((a, b) => a + ' ' + b, '');
+        const search = s.toLowerCase()
+            .replace(/[\u0300-\u036f]/g, "")
+            // .replace(/\(.*\)/gi, '')
+            .replace(/\[.*]/gi, '')
+            .replace(/\|.*/gi, '')
+            .replace(/-/gi, ' ')
+            .replace('feat', '')
+            .replace('psy trance', '')
+            .replace('rmx', '')
+            .replace('remix', '')
+            .replace(' ft. ', ' ')
+            .replace(' vs. ', ' ')
+            .replace(/[^0-9a-zäöëüØß\s\w\u0430-\u044fÆ]/gi, ' ')
+            .replace('official music', '')
+            .replace('official lyric ', '')
+            .replace('official', '')
+            .replace(/lyrics?/g, '')
+            .replace(/\sx\s/g, ' ')
+            .replace('video', '')
+            .replace('free', '')
+            .replace('download', '')
+            .replace(' mix ', '')
+            .replace('original', '')
+            .replace('release', '')
+            .replace('extended', '')
+            .replace('audio', '')
+            .replace(' drum ', ' ')
+            .replace(' bass ', ' ')
+            .replace(/\s+/g, ' ');
+
+        const initWords = [...new Set(search.split(' ').filter(i => i))];
+        const sts = [];
+
+        for (let i = 0; i < initWords.length; i++)
+            sts.push(initWords.slice(i));
+        for (let i = 0; i < initWords.length - 1; i++)
+            sts.push(initWords.slice(0, initWords.length - i - 1));
+
+        sts.sort((a, b) => b.length - a.length);
+
+        let items = [];
+
+        let exMatch = true;
+        for (const sta of sts) {
+            const st = sta.join(' ');
+
+            const {body: {tracks: {items: fi}}} = await spotifyApi.searchTracks(st);
+
+            if (!fi?.length) {
+                exMatch = false;
+                continue;
+            }
+
+            items = fi;
+            break;
+        }
+
+        let added = false;
+        outer: for (const item of items) {
+            for (const {name} of item.artists.reverse()) {
+                if (v.musicMetadata || t.toLowerCase().includes(name.toLowerCase())) {
+                    matches[item.uri] = item;
+                    added = true;
+                    console.log('Add', String(Object.keys(matches).length).padEnd(5), item.name.padEnd(70), t);
+                    break outer;
+                }
+            }
+        }
+
+        if (!added) {
+            if (v.musicMetadata.length)
+                console.log('Miss w. MD', t.padEnd(70), search);
+            else
+                console.log('Miss'.padEnd(10), t.padEnd(70), search);
+        }
+    }
+
+    const list = Object.keys(matches);
+    console.log('Matched', list.length, 'out of', playlist.items.length, ((100 * list.length) / playlist.items.length).toFixed(2) + '%');
+
+    const {body} = await spotifyApi.createPlaylist(title, {description, "public": true});
+    const spotifyPlaylistId = body.id;
+
+    for(let i = 0; i < list.length ; i += 100){
+        const tracks = list.slice(i, i + 100);
+        if(tracks?.length)
+            await spotifyApi.addTracksToPlaylist(spotifyPlaylistId, tracks);
+    }
+
+    console.log('Created Spotify playlist', body.external_urls.spotify);
 }
 
 module.exports = {
