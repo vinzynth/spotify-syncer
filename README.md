ae53092d8ae9e407fc43152c6fccea8b753c2fcc
Flügerl <boostedfluegerl@gmail.com>
2023-01-06 10:08:40 +0100

    Added token setup

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # spotify-syncer
-NodeJS tool to create Spotify playlists from given Youtube playlists
+NodeJS tool to create Spotify playlists from given YouTube playlists.
+
+## Setup
+
+* Log in on [Spotify Developers](https://developer.spotify.com/dashboard/applications) and create
+a new Application.
+* Add (http://localhost:8888/callback) as redirect URI in the application settings.
+* Copy and paste the `clientId` and `clientSecret` to a `credentials.json` file. Format should be as below:
+```json
+{
+    "clientId": "<YOUR_CLIENT_ID>",
+    "clientSecret": "<YOUR_CLIENT_SECRET>"
+}
+```
+* Run `npm run server` or `node src/accessServer`. This will run a webserver locally which requests you to
+sign in with your Spotify account. After successfully signing in, you should see a `tokens.json`
+file which contains an `access_token` and a `refresh_token`. Those are used by the importer to access
+the Spotify api to create playlists and add songs.
+* Note: the server automatically updates the `access_token` based on if's expiration time if you leave it 
+running, but that is optional. You can as well stop it now.
+
+## Usage
+
---
+++ src/accessServer.js
@@ -0,0 +1,88 @@
+const SpotifyWebApi = require('spotify-web-api-node');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 8888;
+const API_SCOPES = [
+    'ugc-image-upload',
+    'user-read-playback-state',
+    'user-modify-playback-state',
+    'user-read-currently-playing',
+    'streaming',
+    'app-remote-control',
+    'user-read-email',
+    'user-read-private',
+    'playlist-read-collaborative',
+    'playlist-modify-public',
+    'playlist-read-private',
+    'playlist-modify-private',
+    'user-library-modify',
+    'user-library-read',
+    'user-top-read',
+    'user-read-playback-position',
+    'user-read-recently-played',
+    'user-follow-read',
+    'user-follow-modify'
+];
+
+const credentialFile = path.resolve(process.argv[2] ?? 'credentials.json');
+
+const spotifyApi = new SpotifyWebApi({
+    redirectUri: `http://localhost:${PORT}/callback`,
+    ...require(credentialFile)
+});
+
+const app = express();
+
+app.get('/login', (req, res) => {
+    res.redirect(spotifyApi.createAuthorizeURL(API_SCOPES));
+});
+
+app.get('/callback', (req, res) => {
+    const error = req.query.error;
+    const code = req.query.code;
+
+    if (error) {
+        console.error('Callback Error:', error);
+        res.send(`Callback Error: ${error}`);
+        return;
+    }
+
+    spotifyApi
+        .authorizationCodeGrant(code)
+        .then(data => {
+            const access_token = data.body['access_token'];
+            const refresh_token = data.body['refresh_token'];
+            const expires_in = data.body['expires_in'];
+
+            spotifyApi.setAccessToken(access_token);
+            spotifyApi.setRefreshToken(refresh_token);
+
+            const tokens = {access_token, refresh_token};
+
+            fs.writeFileSync(path.resolve(__dirname, '..', 'tokens.json'), JSON.stringify(tokens, null, 4));
+
+            console.log(`Successfully retrieved access token. Expires in ${expires_in} s.`);
+            res.send('Success! You can now close the window.');
+
+            setInterval(async () => {
+                const data = await spotifyApi.refreshAccessToken();
+                const access_token = data.body['access_token'];
+
+                spotifyApi.setAccessToken(access_token);
+
+                tokens.access_token = access_token;
+
+                fs.writeFileSync(path.resolve(__dirname, '..', 'tokens.json'), JSON.stringify(tokens, null, 4));
+
+                console.log('The access token has been refreshed!');
+            }, expires_in / 2 * 1000);
+        })
+        .catch(error => {
+            console.error('Error getting Tokens:', error);
+            res.send(`Error getting Tokens: ${error}`);
+        });
+});
+
+app.listen(PORT, () => console.log(`Access Server up. Now log in at http://localhost:${PORT}/login with your browser.`));
